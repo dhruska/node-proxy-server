442c51b3a4b006c20991a565a8aa4de5f867bd47
Donald Hruska <donaldhruska@gmail.com>
2015-04-26 23:49:57 -0500

    Simple proxy server

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let http = require('http')
+let request = require('request')
+let destinationUrl = 'http://127.0.0.1:8000'
 
 http.createServer((req, res) => {
 	for (let header in req.headers) {
 		res.setHeader(header, req.headers[header])
 	}
-	req.pipe(res);
+	req.pipe(res)
 }).listen(8000)
 
 console.log('Listening at http://127.0.0.1:8000')
+
+
+http.createServer((req, res) => {
+	let options = {
+		headers: req.headers,
+		url: destinationUrl
+	}
+	let destinationResponse = req.pipe(request(options))
+	destinationResponse.pipe(res)
+}).listen(8001)
